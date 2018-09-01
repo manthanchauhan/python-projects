13e62a1d2b4876bb8919fa7116b37592f7d4b0e3
manthanchauhan <manthanchauhan913@gmail.com>
2018-09-01 19:31:57 +0530

    minor upgrades using pandas

--- email-sender/sender.py
+++ email-sender/sender.py
@@ -1,35 +1,21 @@
-import csv
+import pandas as pd
 from string import Template
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-datafile_address = 'D:\Git\python-projects\email-sender\data.csv'
-messagefile_address = 'D:\Git\python-projects\email-sender\message.txt'
-my_email = 'myemail@gmail.com'
+#datafile_address = 'D:\Git\python-projects\email-sender\data.csv'
+datafile_address = '/home/manthan/Git/python-projects/email-sender/Book1.csv'
+#messagefile_address = 'D:\Git\python-projects\email-sender\message.txt'
+messagefile_address = '/home/manthan/Git/python-projects/email-sender/message.txt'
+my_email = 'manthanchauhan913@gmail.com'
 my_password = 'mypassword'
 email_subject = 'subject of email'
 
 def read_data():
     filename = datafile_address
-    contacts = []
-    datafile = open(filename, 'r')
-    datareader = csv.reader(datafile)
-    fields = next(datareader)
-    email_pos = -1
-    name_pos = -1
-    l = len(fields)
-    for i in range(0, l):
-        if fields[i] == 'name':
-            name_pos = i
-        elif fields[i] == 'email':
-            email_pos = i
-        if email_pos != -1 and name_pos != -1:
-            break
-    for row in datareader:
-        name = row[name_pos]
-        email = row[email_pos]
-        contacts.append([name, email])
+    dataframe = pd.read_csv(filename)
+    contacts = list(zip(dataframe['name'], dataframe['email']))    
     return contacts
 def read_msg():
     filename = messagefile_address
@@ -40,7 +26,8 @@ def create_channel():
     s = smtplib.SMTP(host='smtp.gmail.com', port=587)
     s.starttls()
     s.login(my_email, my_password)
-    return s    
+    return s  
+  
 contacts = read_data()
 template = read_msg()
 channel = create_channel()
