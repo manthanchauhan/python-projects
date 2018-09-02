4b9d28ec59c5c5b5a3056475fb898d4453b58c79
manthanchauhan <manthanchauhan913@gmail.com>
2018-09-02 07:21:47 +0530

    csv sorter

--- csv sorter/csv sorter.py
+++ csv sorter/csv sorter.py
@@ -1,7 +1,10 @@
 import pandas
+import os
 datafile = 'D:\Git\python-projects\csv sorter\data.csv'
 outputfile = 'D:\Git\python-projects\csv sorter\dataout.csv'
 df = pandas.read_csv(datafile)
 df.sort_values(by='name', axis = 0, ascending=True, inplace= True)
 df.to_csv(outputfile)
+os.remove(datafile)
+os.rename(outputfile, datafile)
 print('Sorted')
