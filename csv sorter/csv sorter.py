db4cde7cafaf0098d9f6d1f9f03f5b911ca301c4
manthanchauhan <manthanchauhan913@gmail.com>
2018-09-02 09:08:23 +0530

    csv sorter

--- csv sorter/csv sorter.py
+++ csv sorter/csv sorter.py
@@ -1,6 +1,8 @@
 import pandas
-datafile = 'D:\Git\python-projects\csv sorter\data.csv'
-outputfile = 'D:\Git\python-projects\csv sorter\dataout.csv'
+#datafile = 'D:\Git\python-projects\csv sorter\data.csv'
+datafile = '/home/manthan/Git/python-projects/email-sender/data.csv'
+#outputfile = 'D:\Git\python-projects\csv sorter\dataout.csv'
+outputfile = '/home/manthan/Git/python-projects/email-sender/data.csv'
 df = pandas.read_csv(datafile)
 df.sort_values(by='name', axis = 0, ascending=True, inplace= True)
 df.to_csv(outputfile)
