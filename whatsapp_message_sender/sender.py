3a013a3eb51cc489291e2d7b43b025cb02178965
manthanchauhan <manthanchauhan913@gmail.com>
2018-09-11 21:14:15 +0530

    whatsapp messenger

--- whatsapp_message_sender/sender.py
+++ whatsapp_message_sender/sender.py
@@ -1,37 +1,41 @@
+import time
+from string import Template
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
- 
-# Replace below path with the absolute path
-# to chromedriver in your computer
-chrome = 'D:\Git\python-projects\whatsapp_message_sender\chromedriver.exe'
-driver = webdriver.Chrome(chrome)
- 
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 600)
- 
-# Replace 'Friend's Name' with the name of your friend 
-# or the name of a group 
-target = '"Madhav"'
-print('here')
- 
-# Replace the below string with your own message
-string = "Message sent using Python!!!"
- 
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, x_arg)))
-print(target + ' located')
-group_title.click()
-time.sleep(5)
-print('searching for text box')
-inp_xpath = '_2S1VP copyable-text selectable-text'
-input_box = wait.until(EC.presence_of_element_located((
-    By.CLASS_NAME, inp_xpath)))
-print('box located')
+import urllib
 
-input_box.send_keys(string + Keys.ENTER)
-time.sleep(1)
+name="Harshita"
+msg_address = 'D:\Git\python-projects\whatsapp_message_sender\message.txt'
+chrome_driver = 'D:\Git\python-projects\whatsapp_message_sender\chromedriver.exe'
+searchbox = '//input[@class="jN-F5 copyable-text selectable-text"]'
+messagebox = '//div[@class="_2S1VP copyable-text selectable-text"]'
+
+def read_message(msg_file):
+    msgfile = open(msg_address, 'r')
+    msg = msgfile.read()
+    return Template(msg)
+def login(chrome_driver):
+    driver = webdriver.Chrome(chrome_driver)
+    driver.get("http://web.whatsapp.com/")
+    searchBox = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH,searchbox)))
+    return driver
+def sendmsg(name, driver, searchbox, messagebox, message):
+    searchBox = driver.find_element_by_xpath(searchbox)
+    searchBox.clear()
+    searchBox.send_keys(name)
+    searchBox.send_keys(Keys.ENTER)
+    messageBox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, messagebox)))
+    message = message.substitute(name= name)
+    message = message.split('\n')
+    for msg in message:
+        messageBox.send_keys(msg)
+        messageBox.send_keys(Keys.SHIFT + Keys.ENTER)
+    messageBox.send_keys(Keys.ENTER)
+
+driver = login(chrome_driver)
+message= read_message(msg_address)
+sendmsg(name, driver, searchbox, messagebox, message)
+driver.quit()
