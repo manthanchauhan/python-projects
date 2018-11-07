34dbc10b4bc9d3a70843ec51f6835cb838b82081
manthanchauhan <manthanchauhan913@gmail.com>
2018-11-07 13:54:26 +0530

    dowloads

--- google drive terminal/uploader.py
+++ google drive terminal/uploader.py
@@ -14,6 +14,7 @@ import http
 SCOPES = 'https://www.googleapis.com/auth/drive'
 warnings.filterwarnings('ignore')
 token = 'D:\Git\python-projects\google drive terminal\\token.json'
+download_path = 'D:\Git\python-projects\google drive terminal\\'
 
 
 class DriveClient(object):
@@ -30,19 +31,24 @@ class DriveClient(object):
         parser = argparse.ArgumentParser(description='command-line suite for google drive.')
         parser.add_argument('command', help='subcommand to run', action='store')    
         args = parser.parse_args(sys.argv[1:2])
-        getattr(self, args.command)()
+        try:
+            getattr(self, args.command)()
+        except AttributeError:
+            print('Invalid command')
+            print('for help use: "Drive client -h" or "DriveClient --help"')
     
     def logout(self):
         try:
             os.remove(token)
         except FileNotFoundError:
             pass
+        print('logged out successfully')
         sys.exit()
 
     def listfiles(self):
         parser = argparse.ArgumentParser(description='view files')
         parser.add_argument('number', help='number of pages to print', action='store', type=int)
-        parser.add_argument('--detailed', '-det', help='print file details', action='store_true')
+        parser.add_argument('-det', '--detailed', help='print file details', action='store_true')
         args = parser.parse_args(sys.argv[2:])
         number = args.number
         result = []
@@ -73,13 +79,13 @@ class DriveClient(object):
                         if tpl != 'name':
                             print('\t' + tpl + ' : ' + str(result[i][tpl]))
             except:
-                print('[' + str(i+1) + '] --------------------------')
+                print('[' + str(i+1) + '] -----<encoding error in filename>-----')
         sys.exit()  
 
     def download(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('filename', help='name of file to be downloaded', action='store')
-        parser.add_argument('adress', help='location to download file', action='store')
+        parser.add_argument('address', help='location to download file, "default" for default location', action='store')
         args = parser.parse_args(sys.argv[2:])
         page_token = None
         fileID = None
@@ -110,53 +116,16 @@ class DriveClient(object):
         done = False
         while not done:
             status, done = downloader.next_chunk()
-            print('Downloaded %d%'%(status.progress()*100))
-        with open(args.adress, 'wb') as File:
+            print('Downloaded %d'%(status.progress()*100))
+        if args.address == 'default':
+            address = download_path + args.filename
+        else:
+            address = args.address    
+        with open(address, 'wb') as File:
             File.write(fh.getvalue())
         File.close()
         print(args.filename + ' was downloaded successfully')
         sys.exit()
 
-
-#     if hasattr(args.filename):
-#         results = get_all_files(service)
-#         for File in results:
-#             if File['name'] == args.filename:
-#                 fileID = File['id']
-#         request = service.files().get_media(fileId=fileID)
-#         fh = io.BytesIO()
-#         downloader = googleapiclient.http.MediaIoBaseDownload(fh, request)
-#         done = False
-#         while not done:
-#             status, done =  downloader.next_chunk()
-#             print('Downloaded %d'%(status.progress()* 100))
-#         with open(args.filename, 'wb') as f:
-#             f.write(fh.getvalue())
-#         fh.close()
-    
-    # Call the Drive v3 API
-    # results = service.files().list(
-    #     pageSize=100, fields="nextPageToken, files(id, name)").execute()
-    # items = results.get('files', [])
-
-    # if not items:
-    #     print('No files found.')
-    # else:
-    #     print('Files:')
-    #     for item in items:
-    #         try:
-    #             print(item['name'])
-    #         except:
-    #             pass
-
 if __name__ == '__main__':
     DriveClient()
-    
-
-
-    # parser_start = subparsers.add_parser('start', help='dummy')
-    # parser_down = subparsers.add_parser('download', help='download some file')
-    # parser_down.add_argument('filename', help='name of file to download', action='store')
-    # parser.add_argument('-lout', '--logout', help='logout the current Drive account', action='store_true')
-    # parser.add_argument('-ls', '--list', help='list all files', action='store_true')
-    # main(args)
